883619e8e522fe63cecbfe3066bea044684e76d6
vincedefreitas <97405426+vincedefreitas@users.noreply.github.com>
2024-09-23 10:23:23 +0100

    Create db and set default attributes

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+
+$db = new PDO(
+    'mysql:host=DB;dbname=collectorapp', // DSN
+    'root', // username
+    'password' // password
+);
+
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);

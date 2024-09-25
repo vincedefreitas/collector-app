6d2033cdb5c326508e897bd486f173ced655405f
vincedefreitas <97405426+vincedefreitas@users.noreply.github.com>
2024-09-25 16:45:56 +0100

    Add functionality to add to collection

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ require_once 'src/functions.php';
 
 $db = connectToDB();
 
+addGameToDB($db);
+
 ?>
 
 <!doctype html>
@@ -22,36 +24,44 @@ $db = connectToDB();
     <h1>Vince's Game Collection</h1>
 </div>
 <div class="form-container">
-    <form action="src/formdata.php" method="post">
-        <label for="game-title">Game Title:</label>
-        <input type="text" id="game-title" name="game-title" placeholder="e.g. Elden Ring">
-        <label for="game-genre">Genre:</label>
-        <select name="game-genre" id="game-genre">
-            <option value="1">Action/Adventure</option>
-            <option value="2">Platformer</option>
-            <option value="3">Fighting</option>
-            <option value="4">Puzzle</option>
-            <option value="5">JRPG</option>
-        </select>
-        <label for="game-platform">Genre:</label>
-        <select name="game-platform" id="game-platform">
-            <option value="1">PS5</option>
-            <option value="2">PS4</option>
-            <option value="3">Xbox Series S</option>
-            <option value="4">Nintendo Switch</option>
-            <option value="5">Nintendo 3DS</option>
-            <option value="6">PS3</option>
-            <option value="7">PC</option>
-            <option value="8">Xbox 360</option>
-        </select>
-        <label for="game-age">Age:</label>
-        <select name="game-age" id="game-age">
-            <option value="1">3</option>
-            <option value="2">7</option>
-            <option value="3">12</option>
-            <option value="4">16</option>
-            <option value="5">18</option>
-        </select>
+    <form action="index.php" method="post">
+        <div class="form-group">
+            <label for="game-title">Game Title:</label>
+            <input type="text" id="game-title" name="game-title" placeholder="e.g. Elden Ring" required>
+        </div>
+        <div class="form-group">
+            <label for="game-genre">Genre:</label>
+            <select name="game-genre" id="game-genre" required>
+                <option value="1">Action/Adventure</option>
+                <option value="2">Platformer</option>
+                <option value="3">Fighting</option>
+                <option value="4">Puzzle</option>
+                <option value="5">JRPG</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="game-platform">Genre:</label>
+            <select name="game-platform" id="game-platform" required>
+                <option value="1">PS5</option>
+                <option value="2">PS4</option>
+                <option value="3">Xbox Series S</option>
+                <option value="4">Nintendo Switch</option>
+                <option value="5">Nintendo 3DS</option>
+                <option value="6">PS3</option>
+                <option value="7">PC</option>
+                <option value="8">Xbox 360</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="game-age">Age:</label>
+            <select name="game-age" id="game-age" required>
+                <option value="1">3</option>
+                <option value="2">7</option>
+                <option value="3">12</option>
+                <option value="4">16</option>
+                <option value="5">18</option>
+            </select>
+        </div>
         <input type="submit" value="Add to Collection">
     </form>
 </div>
--- src/functions.php
+++ src/functions.php
@@ -17,12 +17,20 @@ function getAllGames(object $db): array
     return $videogames;
 }
 
+function checkImgExists(array $game): string {
+    if (!$game['image']) {
+        return "";
+    } else {
+        return "<img class='card-img' src='{$game['image']}' alt='Image of {$game['name']} cover'>";
+    }
+}
 function displayAllGames(array $games): string
 {
     $result = "";
     foreach ($games as $game) {
+        $img = checkImgExists($game);
         $result .= "<div class='card'>
-        <img class='card-img' src='{$game['image']}' alt='Image of {$game['name']} cover'>
+        {$img}
         <h2>Game: {$game['name']}</h2>
         <p><strong>Genre:</strong> {$game['genre']}</p>
         <p><strong>Platform:</strong> {$game['platform']}</p>
@@ -30,4 +38,40 @@ function displayAllGames(array $games): string
     </div>";
     }
     return $result;
+}
+
+function getTable(string $tablename, PDO $db) {
+    $query = $db->prepare("SELECT `id`, `{$tablename}` FROM `{$tablename}`");
+    $result = $query->execute();
+    if ($result) {
+        return $query->fetchAll();
+    }
+}
+
+function setDropDownOptions(array $table, $column): string {
+    $options= '';
+    foreach($table as $item) {
+        $options .= "<option value=\"{$item['id']}\">{$item[$column]}</option>";
+    }
+    return $options;
+}
+
+function addGameToDB($db): void {
+    if(isset($_POST['game-title']) && isset($_POST['game-genre']) && isset($_POST['game-platform']) && isset($_POST['game-age'])) {
+        $gametitle = $_POST['game-title'];
+        $gamegenre = $_POST['game-genre'];
+        $gameplatform = $_POST['game-platform'];
+        $gameage = $_POST['game-age'];
+
+        $query = $db->prepare('INSERT INTO `videogames` (`name`, `genreid`, `platformid`, `ageid`)
+    VALUES (:gametitle, :gamegenre, :gameplatform, :gameage);'
+        );
+
+        $query->execute([
+            'gametitle' => $gametitle,
+            'gamegenre' => $gamegenre,
+            'gameplatform' => $gameplatform,
+            'gameage' => $gameage
+        ]);
+    }
 }

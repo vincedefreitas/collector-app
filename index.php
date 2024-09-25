186dcab29a7631df43e86d7137c1161a8c685963
vincedefreitas <97405426+vincedefreitas@users.noreply.github.com>
2024-09-25 11:31:24 +0100

    Add form to index.php

--- index.php
+++ index.php
@@ -21,7 +21,40 @@ $db = connectToDB();
 <div class="title">
     <h1>Vince's Game Collection</h1>
 </div>
-
+<div class="form-container">
+    <form action="src/formdata.php" method="post">
+        <label for="game-title">Game Title:</label>
+        <input type="text" id="game-title" name="game-title" placeholder="e.g. Elden Ring">
+        <label for="game-genre">Genre:</label>
+        <select name="game-genre" id="game-genre">
+            <option value="1">Action/Adventure</option>
+            <option value="2">Platformer</option>
+            <option value="3">Fighting</option>
+            <option value="4">Puzzle</option>
+            <option value="5">JRPG</option>
+        </select>
+        <label for="game-platform">Genre:</label>
+        <select name="game-platform" id="game-platform">
+            <option value="1">PS5</option>
+            <option value="2">PS4</option>
+            <option value="3">Xbox Series S</option>
+            <option value="4">Nintendo Switch</option>
+            <option value="5">Nintendo 3DS</option>
+            <option value="6">PS3</option>
+            <option value="7">PC</option>
+            <option value="8">Xbox 360</option>
+        </select>
+        <label for="game-age">Age:</label>
+        <select name="game-age" id="game-age">
+            <option value="1">3</option>
+            <option value="2">7</option>
+            <option value="3">12</option>
+            <option value="4">16</option>
+            <option value="5">18</option>
+        </select>
+        <input type="submit" value="Add to Collection">
+    </form>
+</div>
 <div class="card-container">
     <?php
         try {
